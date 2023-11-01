f78d0d0f8e9ebb699621b9f6626a3c45d5ef13e7
csndvl <chrisandoval10@gmail.com>
2023-11-01 19:04:59 +0000

    Retrieves product data from s3

--- data_extraction.py
+++ data_extraction.py
@@ -4,6 +4,8 @@ import tabula as tb
 import requests
 import json
 import numpy as np
+import boto3
+from io import BytesIO
 
 
 class DataExtractor():
@@ -27,22 +29,43 @@ class DataExtractor():
         return (card_details)
     
     def list_number_of_stores(self, endpoint, api_key):
+        '''The function counts how many stores in the data'''
         print("running list_number_of_stores...")
         response = requests.get(endpoint, headers = api_key).content
-        result = json.loads(response)
+        result = json.loads(response) # Loads is used to convert JSON string into python dictionary
         result = result['number_stores']
         print ("done")
         return (result) # result = 451
     
     def retrieve_stores_data(self, endpoint, num_stores, api_key):
+        '''The function collects all store data using its store numbers'''
         print("running retrieve_stores_data")
         store_details = []
-        for stores in range(0, num_stores):
+        for stores in range(0, int(num_stores)): # Iterates from 0 to the total of stores
             response = requests.get(f"{endpoint}{stores}", headers = api_key).json()
             store_details.append(pd.DataFrame(response, index = [np.NaN]))
-        store_details_df = pd.concat(store_details)
+        store_details_df = pd.concat(store_details) # Put all multiple dataframe inside store_detail into one
         print ("done")
         return (store_details_df)
+    
+    def extract_from_s3(self, address):
+        print ("running extract from s3")
+        client = boto3.client('s3') #
+        split_address = address.replace("s3://", "").split("/")
+        bucket_name = split_address[0]
+        s3key_path = "/".join(split_address[1:])
+
+        data = client.get_object(Bucket = bucket_name, Key = s3key_path)
+        data =  data['Body'].read()
+
+        df = pd.read_csv(BytesIO(data)) # Creates an in-memory buffer for the data retrieved from the S3 bucket and can be read by read_csv
+        print (df.head())
+
+    
+        print ("done")
+
+
+        pass
        
       
 
@@ -56,7 +79,11 @@ test1 = DataExtractor()
 # link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
 # test1.retrieve_pdf_data()
 
-number_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-store_details_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/"
-api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
-test1.retrieve_stores_data(store_details_endpoint, number_of_stores_endpoint, api_key)
+# number_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
+# store_details_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/"
+# api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+# test1.retrieve_stores_data(store_details_endpoint, number_of_stores_endpoint, api_key)
+address = "s3://data-handling-public/products.csv"
+test1.extract_from_s3(address)
+
+
