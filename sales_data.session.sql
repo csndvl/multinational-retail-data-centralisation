484a2aba2e25af5be3523de88eec67e36bea7d96
csndvl <chrisandoval10@gmail.com>
2023-11-03 13:26:07 +0000

    Correct dim store details table's column type

--- sales_data.session.sql
+++ sales_data.session.sql
@@ -79,6 +79,19 @@ UPDATE dim_store_details
 SET locality = NULL
 WHERE locality = 'N/A'
 
+-- Alter column data types
+ALTER TABLE dim_store_details
+ALTER COLUMN locality TYPE VARCHAR(255),
+ALTER COLUMN store_code TYPE VARCHAR(12),
+ALTER COLUMN staff_numbers TYPE SMALLINT,
+ALTER COLUMN opening_date TYPE DATE,
+ALTER COLUMN store_type TYPE VARCHAR(255),
+ALTER COLUMN country_code TYPE VARCHAR(2),
+ALTER COLUMN continent TYPE VARCHAR(255),
+ALTER COLUMN longitude TYPE FLOAT
+USING longitude::FLOAT,
+ALTER COLUMN latitude TYPE FLOAT
+USING latitude::FLOAT;
 
 
 
