87d6b0fa9ae42fa776a9b6f8dec084f272a5e92a
csndvl <chrisandoval10@gmail.com>
2023-11-03 10:55:28 +0000

    Corrects order table column types

--- sales_data.session.sql
+++ sales_data.session.sql
@@ -24,21 +24,8 @@ ALTER COLUMN product_code TYPE VARCHAR(11),
 ALTER COLUMN product_quantity TYPE SMALLINT;
 
 
---TASK 2: Change dim user table into the correct data types
 
---Find the longest country code length
-SELECT MAX(LENGTH(country_code::text)) FROM dim_user
-SET LIMIT 1; --2
 
--- Alter column data types
-ALTER TABLE dim_user
-ALTER COLUMN first_name TYPE VARCHAR(255),
-ALTER COLUMN last_name TYPE VARCHAR(255),
-ALTER COLUMN date_of_birth TYPE DATE,
-ALTER COLUMN country_code TYPE VARCHAR(2),
-ALTER COLUMN user_uuid TYPE UUID
-USING user_uuid::uuid,
-ALTER COLUMN join_date TYPE DATE;
 
 
 
