fe941b2df19fb9c88d05074e572aee9606901f7e
csndvl <chrisandoval10@gmail.com>
2023-11-03 10:51:18 +0000

    Corrects dim user's column types

--- sales_data.session.sql
+++ sales_data.session.sql
@@ -1,6 +1,47 @@
---Task 1
+--TASK 1: Change order table columns into the correct data types
 
-SELECT * FROM order_table;
+-- Find the longest card number length
+SELECT MAX(LENGTH(card_number::TEXT)) FROM order_table
+SET LIMIT 1; --19
 
-ALTER TABLE table_name
-ALTER COLUMN column_name datatype;
+-- Find the longest store code length
+SELECT MAX(LENGTH(store_code::TEXT)) FROM order_table
+SET LIMIT 1; --12
+
+-- Find the longest product code length
+SELECT MAX(LENGTH(product_code::TEXT)) FROM order_table
+SET LIMIT 1; --11
+
+-- Alter column data types
+ALTER TABLE order_table
+ALTER COLUMN date_uuid TYPE UUID
+USING date_uuid::uuid,
+ALTER COLUMN user_uuid TYPE UUID
+USING user_uuid::uuid,
+ALTER COLUMN card_number TYPE VARCHAR(19),
+ALTER COLUMN store_code TYPE VARCHAR(12),
+ALTER COLUMN product_code TYPE VARCHAR(11),
+ALTER COLUMN product_quantity TYPE SMALLINT;
+
+
+--TASK 2: Change dim user table into the correct data types
+
+--Find the longest country code length
+SELECT MAX(LENGTH(country_code::text)) FROM dim_user
+SET LIMIT 1; --2
+
+-- Alter column data types
+ALTER TABLE dim_user
+ALTER COLUMN first_name TYPE VARCHAR(255),
+ALTER COLUMN last_name TYPE VARCHAR(255),
+ALTER COLUMN date_of_birth TYPE DATE,
+ALTER COLUMN country_code TYPE VARCHAR(2),
+ALTER COLUMN user_uuid TYPE UUID
+USING user_uuid::uuid,
+ALTER COLUMN join_date TYPE DATE;
+
+
+
+
+
+
