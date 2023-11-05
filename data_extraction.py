aeb758a59d58a35ed736b0ba545224005e473d69
csndvl <chrisandoval10@gmail.com>
2023-11-05 18:27:59 +0000

    Adds docstrings and in-line comments

--- data_extraction.py
+++ data_extraction.py
@@ -9,7 +9,7 @@ from io import BytesIO
 
 
 class DataExtractor():
-    '''Extracts data from the database.'''     
+    '''This function extracts data from different sources'''     
 
     def read_rds_table(self, db_con, table_name): 
         """The function reads a table from the RDS database and prints it as a dataframe."""
@@ -17,7 +17,7 @@ class DataExtractor():
         data = pd.read_sql_table(table_name, db_con)
         df = pd.DataFrame(data)
         print ("done")
-        return(df)
+        return (df)
     
     def retrieve_pdf_data(self):
         '''The function reads card details inside the pdf file'''
@@ -68,30 +68,6 @@ class DataExtractor():
             df = pd.read_csv(BytesIO(data)) # Creates an in-memory buffer for the data retrieved from the S3 bucket and can be read by read_csv
         elif 'json' in s3key_path:
             df = pd.read_json(BytesIO(data)) # Creates an in-memory buffer for the data retrieved from the S3 bucket and can be read by read_json
-        print ("done")
+        print ("done\n")
         return (df)
     
-  
-
-        
-       
-      
-
-
-
-# db = db_con()
-# #creds = db.read_db_creds()
-# engine = db.init_db_engine()
-test1 = DataExtractor()
-#test1.read_rds_table(engine, "legacy_users")
-# link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
-# test1.retrieve_pdf_data()
-
-# number_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-# store_details_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/"
-# api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
-# test1.retrieve_stores_data(store_details_endpoint, number_of_stores_endpoint, api_key)
-# address = "s3://data-handling-public/products.csv"
-# test1.extract_from_s3(address)
-
-
